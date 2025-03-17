4d53f380dc18adf03d2eeef8bd982c17d9e2b3d4
Turgho <victor.hugo3692111@gmail.com>
2025-03-17 18:41:15 -0300

    add: auth controller

---
+++ src/modules/auth/controllers/auth.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from '../services/auth.service';
+import { LoginDto } from '../dto/login.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
+
+@ApiTags('Users')
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  
+  // Rota para login de usuário
+  @Post('login')
+  @ApiOperation({ summary: 'User login.' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'User has been successfully logged in.',
+    schema: { 
+      example: { access_token: 'your_jwt_token_here' } 
+    }
+  })
+  @ApiResponse({ 
+    status: 401, 
+    description: 'Unauthorized user login.',
+    schema: { 
+      example: { message: 'Invalid credentials' } 
+    }
+  })
+  @ApiResponse({ 
+    status: 404, 
+    description: 'User not found.',
+    schema: { 
+      example: { message: 'User not found' } 
+    }
+  })
+  async login(@Body() loginDto: LoginDto) {
+    try {
+      return await this.authService.login(loginDto);
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      throw error;
+    }
+  }
+}
