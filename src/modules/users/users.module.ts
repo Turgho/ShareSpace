46d4f442ce8a20645ba7547da9ba1f590d239000
Turgho <victor.hugo3692111@gmail.com>
2025-03-20 22:44:37 -0300

    fix: missing modules

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './controllers/users.controller';
+import { JwtStrategy } from 'src/middleware/jwt/jwt.strategy';
+import { JwtAuthGuard } from 'src/middleware/jwt/jwt-auth.guard';
 import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
 import { PrismaService } from '../database/prisma.service';
 import { PasswordHasherService } from '../auth/services/password-hasher.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaUserRepository } from './repositories/user.repository';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,  // Defina a chave secreta
+      signOptions: { expiresIn: '1h' },  // Defina o tempo de expiração do token
+    }),
+  ],
+  providers: [
+    UsersService, 
+    JwtStrategy, 
+    JwtAuthGuard, 
+    PrismaService, 
+    PasswordHasherService,
+    {
+      provide: 'UserRepository',
+      useClass: PrismaUserRepository,
+    }
+  ],
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, PasswordHasherService],
-  exports: [UsersService],
 })
 export class UsersModule {}
