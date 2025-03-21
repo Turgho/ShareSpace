209313f45192510c37b408fc2f7368a575b4b59c
Turgho <victor.hugo3692111@gmail.com>
2025-03-20 22:44:20 -0300

    add: password-hasher to SOLID

--- src/modules/auth/services/password-hasher.service.ts
+++ src/modules/auth/services/password-hasher.service.ts
@@ -1,12 +1,21 @@
-// src/modules/auth/services/password-hasher.service.ts
-import { Injectable } from '@nestjs/common';
+// password-hasher.service.ts
 import * as bcrypt from 'bcrypt';
-import { IPasswordHasher } from '../interfaces/password-hasher.interface';
+import { Injectable } from '@nestjs/common';
+
+export interface PasswordHasher {
+  hashPassword(password: string): Promise<string>;
+  comparePasswords(password: string, hashedPassword: string): Promise<boolean>;
+}
 
 @Injectable()
-export class PasswordHasherService implements IPasswordHasher {
+export class PasswordHasherService implements PasswordHasher {
   async hashPassword(password: string): Promise<string> {
+    // Gera um salt para o hash da senha e retorna a senha com hash
     const salt = await bcrypt.genSalt();
     return bcrypt.hash(password, salt);
+  }
+
+  async comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
   }
 }
